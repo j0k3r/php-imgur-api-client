1a96178eb537a3889f74b35bb87c112b171c32e0
Adyg <ady.ghiuta@gmail.com>
2014-02-10 15:24:44 +0200

    added Memegen api support

--- lib/Imgur/Api/Memegen.php
+++ lib/Imgur/Api/Memegen.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace Imgur\Api;
+
+use Imgur\Api\AbstractApi;
+
+/**
+ * CRUD for Memegen
+ * 
+ * @link https://api.imgur.com/endpoints/memegen
+ * @author Adrian Ghiuta <adrian.ghiuta@gmail.com>
+ */
+
+class Memegen extends AbstractApi {
+    
+    /**
+     * Get the list of default memes.
+     * 
+     * @return \Imgur\Api\Model\Image|array
+     */
+    public function defaultMemes() {
+        $parameters = $this->get('memegen/defaults');
+        
+        $images = array();
+        
+        foreach($parameters['data'] as $parameter) {
+            $images[] = new Model\Image($parameter);
+        }
+        
+        return $images;
+    }
+}
